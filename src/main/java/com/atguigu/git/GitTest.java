9bbb6b427c82de206eb00bbd12d8265c7ce007d0
yxx <17695632435@163.com>
2021-06-10 22:24:44 +0800

    初次提交

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author yxx
+ * @create 2021-06-10 22:18
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
