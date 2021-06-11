187f050d1c8bd1457b19d9c329c692cd2ce07e6e
yxx <17695632435@163.com>
2021-06-11 13:02:20 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git3");
-        System.out.println("hello git4");
+        System.out.println("hello git46666666666666666");
     }
 }
